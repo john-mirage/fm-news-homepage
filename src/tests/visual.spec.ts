7cf66171245ed947f6e1b01871939ff33e0aa3df
john-mirage <chaosdynamix@posteo.net>
2022-10-26 10:23:27 +0200

    fix: fix some styles to match design

--- src/tests/visual.spec.ts
+++ src/tests/visual.spec.ts
@@ -15,26 +15,34 @@ test.afterEach(async () => {
   });
 });
 
-test.use({ viewport: { width: 1440, height: 1024 } });
+test.describe("desktop", () => {
+  test.use({ viewport: { width: 1440, height: 1024 } });
 
-test("dekstop", async ({ page }) => {
-  await expect(page).toHaveScreenshot("desktop.png", {
-    animations: "disabled",
+  test.only("initial", async ({ page }) => {
+    await expect(page).toHaveScreenshot("desktop.png", {
+      animations: "disabled",
+    });
   });
 });
 
-test.use({ viewport: { width: 375, height: 1872 } });
+test.describe("mobile", () => {
+  test.use({ viewport: { width: 375, height: 1872 } });
 
-test("mobile", async ({ page }) => {
-  await expect(page).toHaveScreenshot("mobile.png", { animations: "disabled" });
+  test("initial", async ({ page }) => {
+    await expect(page).toHaveScreenshot("mobile.png", {
+      animations: "disabled",
+    });
+  });
 });
 
-test.use({ viewport: { width: 375, height: 669 } });
+test.describe("mobile menu", () => {
+  test.use({ viewport: { width: 375, height: 669 } });
 
-test.only("mobile menu", async ({ page }) => {
-  const button = page.locator('role=button[name="open the sidebar"]');
-  await button.click();
-  await expect(page).toHaveScreenshot("mobile-menu.png", {
-    animations: "disabled",
+  test("initial", async ({ page }) => {
+    const button = page.locator('role=button[name="open the sidebar"]');
+    await button.click();
+    await expect(page).toHaveScreenshot("mobile-menu.png", {
+      animations: "disabled",
+    });
   });
 });
